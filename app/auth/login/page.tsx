42cbdb5b8fcc5988a25cd80c1cf2929ede3abcdf
malsain57 <hsaintal@gmail.com>
2026-05-05 15:19:17 +0200

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../../lib/supabase'
 
 export default function LoginPage(){
   const [email,setEmail]=useState(''); const [password,setPassword]=useState(''); const [msg,setMsg]=useState('')
